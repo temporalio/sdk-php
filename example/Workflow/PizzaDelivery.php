7fef963229e16a00d2f2ea882a8e1509497487a5
SerafimArts <nesk@xakep.ru>
2020-09-14 20:09:48 +0300

    Improve example

--- example/Workflow/PizzaDelivery.php
+++ example/Workflow/PizzaDelivery.php
@@ -14,13 +14,54 @@ namespace App\Workflow;
 use Temporal\Client\Declaration\Workflow;
 use Temporal\Client\Declaration\WorkflowInterface;
 use Temporal\Client\Runtime\WorkflowContextInterface;
+use Temporal\Client\Transport\Request\Request;
+use Temporal\Client\Transport\Request\RequestInterface;
+use Temporal\Client\Transport\TransportInterface;
 
 class PizzaDelivery
 {
-    public function handler(WorkflowContextInterface $context)
+    /**
+     * @param TransportInterface $transport
+     * @param WorkflowContextInterface $context
+     */
+    public function handler(TransportInterface $transport, WorkflowContextInterface $context): void
     {
+        $transport->send(
+            $this->executeActivityRequest('ExampleActivity', $context)
+        )
+            ->then(fn() => $transport->send(
+                $this->completeRequest($context)
+            ))
+        ;
+    }
 
-        return ['runid' => $context->getRunId()];
+    /**
+     * @param string $name
+     * @param WorkflowContextInterface $ctx
+     * @return RequestInterface
+     */
+    private function executeActivityRequest(string $name, WorkflowContextInterface $ctx): RequestInterface
+    {
+        return new Request(
+            'ExecuteActivity', [
+                'name' => $name,
+                'rid'  => $ctx->getRunId(),
+            ]
+        );
+    }
+
+    /**
+     * @param WorkflowContextInterface $ctx
+     * @return RequestInterface
+     */
+    private function completeRequest(WorkflowContextInterface $ctx): RequestInterface
+    {
+        return new Request(
+            'CompleteWorkflow', [
+                'rid'    => $ctx->getRunId(),
+                'result' => 'yo!',
+            ]
+        );
     }
 
     /**
