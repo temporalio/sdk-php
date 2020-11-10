0f837af5feb822eceed0a7523e2a254d383aaca2
SerafimArts <nesk@xakep.ru>
2020-11-10 10:13:52 +0300

    Add specific declarations

---
+++ src/Internal/Declaration/ActivityDeclaration.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * This file is part of Temporal package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Temporal\Client\Internal\Declaration;
+
+use Temporal\Client\Activity\Meta\ActivityInterface;
+use Temporal\Client\Activity\Meta\ActivityMethod;
+use Temporal\Client\Workflow\Meta\WorkflowInterface;
+use Temporal\Client\Workflow\Meta\WorkflowMethod;
+
+class ActivityDeclaration extends HandledDeclaration
+{
+    /**
+     * @param ActivityInterface $meta
+     * @param ActivityMethod $method
+     * @param \ReflectionFunctionAbstract $handler
+     */
+    public function __construct(ActivityInterface $meta, ActivityMethod $method, \ReflectionFunctionAbstract $handler)
+    {
+        parent::__construct($meta, $method, $handler);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getMetadata(): ActivityInterface
+    {
+        $result = parent::getMetadata();
+
+        assert($result instanceof ActivityInterface, 'Postcondition failed');
+
+        return $result;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getMethod(): ActivityMethod
+    {
+        $result = parent::getMethod();
+
+        assert($result instanceof ActivityMethod, 'Postcondition failed');
+
+        return $result;
+    }
+}
