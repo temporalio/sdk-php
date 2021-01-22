05ed9a6113dee7e01c7ced7de44eb169474a68bf
SerafimArts <nesk@xakep.ru>
2021-01-22 19:29:51 +0300

    Add doctrine annotations bug workaround

--- src/Workflow/WorkflowInterface.php
+++ src/Workflow/WorkflowInterface.php
@@ -19,6 +19,16 @@ use Spiral\Attributes\NamedArgumentConstructorAttribute;
  * @Target({ "CLASS" })
  */
 #[\Attribute(\Attribute::TARGET_CLASS)]
-final class WorkflowInterface implements NamedArgumentConstructorAttribute
+final class WorkflowInterface /** implements NamedArgumentConstructorAttribute */
+    //
+    // Note: We can not implement "NamedArgumentConstructorAttribute" interface
+    // on attribute classes without "__construct" method.
+    //
+    // Otherwise, we may receive a NPE inside DocParser class, like:
+    //  > Call to a member function getParameters() on null
+    //  > in vendor/doctrine/annotations/lib/Doctrine/Common/Annotations/DocParser.php:518
+    //
+    // Bug is relevant for doctrine/annotations: 1.11 or lower on PHP 7.4 or lower.
+    //
 {
 }
