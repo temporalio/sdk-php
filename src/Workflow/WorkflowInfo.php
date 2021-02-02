eaac14004b58a2bacc11c8d3ec5b37b97c688f62
SerafimArts <nesk@xakep.ru>
2021-02-02 17:01:47 +0300

    Simplify annotations

--- src/Workflow/WorkflowInfo.php
+++ src/Workflow/WorkflowInfo.php
@@ -13,6 +13,7 @@ namespace Temporal\Workflow;
 
 use Carbon\CarbonInterval;
 use Cron\CronExpression;
+use JetBrains\PhpStorm\Immutable;
 use Temporal\Client\ClientOptions;
 use Temporal\Internal\Marshaller\Meta\Marshal;
 use Temporal\Internal\Marshaller\Type\ArrayType;
@@ -22,59 +23,46 @@ use Temporal\Internal\Marshaller\Type\NullableType;
 use Temporal\Internal\Marshaller\Type\ObjectType;
 use Temporal\Worker\WorkerFactoryInterface;
 
+#[Immutable]
 final class WorkflowInfo
 {
     /**
-     * @readonly
-     * @psalm-allow-private-mutation
      * @var WorkflowExecution
      */
     #[Marshal(name: 'WorkflowExecution', type: ObjectType::class, of: WorkflowExecution::class)]
     public WorkflowExecution $execution;
 
     /**
-     * @readonly
-     * @psalm-allow-private-mutation
      * @var WorkflowType
      */
     #[Marshal(name: 'WorkflowType', type: ObjectType::class, of: WorkflowType::class)]
     public WorkflowType $type;
 
     /**
-     * @readonly
-     * @psalm-allow-private-mutation
      * @var string
      */
     #[Marshal(name: 'TaskQueueName')]
     public string $taskQueue = WorkerFactoryInterface::DEFAULT_TASK_QUEUE;
 
     /**
-     * @readonly
-     * @psalm-allow-private-mutation
      * @var \DateInterval
      */
     #[Marshal(name: 'WorkflowExecutionTimeout', type: DateIntervalType::class)]
     public \DateInterval $executionTimeout;
 
     /**
-     * @readonly
-     * @psalm-allow-private-mutation
      * @var \DateInterval
      */
     #[Marshal(name: 'WorkflowRunTimeout', type: DateIntervalType::class)]
     public \DateInterval $runTimeout;
 
     /**
-     * @readonly
-     * @psalm-allow-private-mutation
      * @var \DateInterval
      */
     #[Marshal(name: 'WorkflowTaskTimeout', type: DateIntervalType::class)]
     public \DateInterval $taskTimeout;
 
     /**
-     * @readonly
-     * @psalm-allow-private-mutation
      * @var string
      */
     #[Marshal(name: 'Namespace')]
@@ -84,64 +72,48 @@ final class WorkflowInfo
      * Attempt starts from 1 and increased by 1 for every retry
      * if retry policy is specified.
      *
-     * @readonly
-     * @psalm-allow-private-mutation
      * @var positive-int
      */
     #[Marshal(name: 'Attempt')]
     public int $attempt = 1;
 
     /**
-     * @readonly
-     * @psalm-allow-private-mutation
      * @var CronExpression|null
      */
     #[Marshal(name: 'CronSchedule', type: NullableType::class, of: CronType::class)]
     public ?CronExpression $cronSchedule = null;
 
     /**
-     * @readonly
-     * @psalm-allow-private-mutation
      * @var string|null
      */
     #[Marshal(name: 'ContinuedExecutionRunID')]
     public ?string $continuedExecutionRunId = null;
 
     /**
-     * @readonly
-     * @psalm-allow-private-mutation
      * @var string|null
      */
     #[Marshal(name: 'ParentWorkflowNamespace')]
     public ?string $parentNamespace = null;
 
     /**
-     * @readonly
-     * @psalm-allow-private-mutation
      * @var WorkflowExecution|null
      */
     #[Marshal(name: 'ParentWorkflowExecution', type: NullableType::class, of: WorkflowExecution::class)]
     public ?WorkflowExecution $parentExecution = null;
 
     /**
-     * @readonly
-     * @psalm-allow-private-mutation
      * @var array|null
      */
     #[Marshal(name: 'SearchAttributes', type: NullableType::class, of: ArrayType::class)]
     public ?array $searchAttributes = null;
 
     /**
-     * @readonly
-     * @psalm-allow-private-mutation
      * @var array|null
      */
     #[Marshal(name: 'SearchAttributes', type: NullableType::class, of: ArrayType::class)]
     public ?array $memo = null;
 
     /**
-     * @readonly
-     * @psalm-allow-private-mutation
      * @var string
      */
     #[Marshal(name: 'BinaryChecksum')]
