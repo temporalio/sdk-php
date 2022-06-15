e9c9c5a4076c9de9b6c254d122c6ff1ceaaff4d1
sergeyzhuk <seregazhuk88@gmail.com>
2022-06-15 14:47:37 +0300

    Remove unused code from test

--- tests/Functional/Client/AsyncClosureTestCase.php
+++ tests/Functional/Client/AsyncClosureTestCase.php
@@ -12,17 +12,7 @@ declare(strict_types=1);
 namespace Temporal\Tests\Functional\Client;
 
 use Temporal\Client\WorkflowOptions;
-use Temporal\DataConverter\Type;
-use Temporal\Exception\Client\WorkflowFailedException;
-use Temporal\Exception\Failure\ActivityFailure;
-use Temporal\Exception\Failure\ApplicationFailure;
-use Temporal\Exception\Failure\CanceledFailure;
-use Temporal\Tests\Workflow\AggregatedWorkflow;
 use Temporal\Tests\Workflow\AsyncClosureWorkflow;
-use Temporal\Tests\Workflow\LoopWithSignalCoroutinesWorkflow;
-use Temporal\Tests\Workflow\LoopWorkflow;
-use Temporal\Tests\Workflow\WaitWorkflow;
-use Temporal\Workflow\WorkflowStub;
 
 /**
  * @group client
