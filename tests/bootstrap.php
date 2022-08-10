10e2ecd791db025622f8912030ef721ff6e3de44
Николай Шангин <shanginn@gmail.com>
2022-08-11 00:49:47 +0800

    Stop tests and display error if RoadRunner start failed (#219)
    * Stop tests and display error if RoadRunner start failed
    
    RoadRunner could fail to start for many reasons, for example:
    - rr binary doesn't exist (but in that case we could download it I suppose)
    - config file not found
    - error in the worker setup
    
    It is useful to stop the tests right away and display the error, otherwise the tests are just hang
    
    * Update CI. Add "Downdload RoadRunner" stage in the feature and unit tests
    
    * Restructure GitHub workflows, add more checks, remove duplicate code. Update some dependancies (#3)
    
    * Up the cache action version, add same cache key to 'code-style' workflow
    
    * Don't run temporal test server and rr in unit tests
    
    * Update tests README: add info about conditional test server start
    
    Co-authored-by: Sergey Zhuk <seregazhuk88@gmail.com>

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,6 +6,9 @@ use Temporal\Testing\Environment;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$environment = Environment::create();
-$environment->start('./rr serve -c .rr.silent.yaml -w tests');
-register_shutdown_function(fn () => $environment->stop());
+if (getenv('RUN_TEMPORAL_TEST_SERVER') !== false) {
+    $environment = Environment::create();
+    $environment->start('./rr serve -c .rr.silent.yaml -w tests');
+    register_shutdown_function(fn() => $environment->stop());
+}
+
