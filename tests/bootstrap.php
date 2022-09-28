6a40e81ba7125ee32fbb242599f65ddff649500a
Kajetan <cv65kr@gmail.com>
2022-09-28 21:33:17 +0200

    Fix tests bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,9 +8,10 @@ use Temporal\Tests\SearchAttributeTestInvoker;
 require __DIR__ . '/../vendor/autoload.php';
 
 if (getenv('RUN_TEMPORAL_TEST_SERVER') !== false) {
-    (new SearchAttributeTestInvoker)();
     $environment = Environment::create();
-    $environment->start('./rr serve -c .rr.silent.yaml -w tests');
+    $environment->startTemporalTestServer();
+    (new SearchAttributeTestInvoker)();
+    $environment->startRoadRunner('./rr serve -c .rr.silent.yaml -w tests');
     register_shutdown_function(fn() => $environment->stop());
 }
 
