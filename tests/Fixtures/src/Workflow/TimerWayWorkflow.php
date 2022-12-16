cb78de28be68c7bc2e928445dbcd9a2932b3a97f
roxblnfk <roxblnfk@ya.ru>
2022-12-16 19:07:36 +0300

    Add test case with `wf::timer` and `wf::await` to confirm that `WorkflowContext::$timer` isn't necessary now

---
+++ tests/Fixtures/src/Workflow/TimerWayWorkflow.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * This file is part of Temporal package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Temporal\Tests\Workflow;
+
+use Temporal\Workflow;
+use Temporal\Workflow\WorkflowMethod;
+
+#[Workflow\WorkflowInterface]
+class TimerWayWorkflow
+{
+    #[WorkflowMethod(name: 'TimerWayWorkflow')]
+    public function handler(): iterable
+    {
+        $timerResolved = false;
+
+        $timer = Workflow::timer(20)
+            ->then(function () use (&$timerResolved) {
+                $timerResolved = true;
+            });
+
+        yield Workflow::await($timer, fn() => true);
+
+        return $timerResolved;
+    }
+}
