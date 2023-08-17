a654a8728ff022da9bbaf176674d650b89eab634
Maxim Smakouz <m.smakouz@gmail.com>
2023-08-17 14:37:46 +0300

    Fix version checker unit tests (#342)

--- tests/Unit/Worker/Transport/RoadRunnerTestCase.php
+++ tests/Unit/Worker/Transport/RoadRunnerTestCase.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Unit\Worker\Transport;
 
 use RoadRunner\VersionChecker\Version\ComparatorInterface;
+use RoadRunner\VersionChecker\Version\InstalledInterface;
+use RoadRunner\VersionChecker\Version\RequiredInterface;
 use RoadRunner\VersionChecker\VersionChecker;
 use Temporal\Tests\Unit\UnitTestCase;
 use Temporal\Worker\Transport\RoadRunner;
@@ -17,14 +19,30 @@ final class RoadRunnerTestCase extends UnitTestCase
 {
     public function testCreateShouldCallVersionCheck(): void
     {
+        $installed = $this->createMock(InstalledInterface::class);
+        $installed
+            ->expects($this->once())
+            ->method('getInstalledVersion')
+            ->willReturn('2023.1.0');
+
+        $required = $this->createMock(RequiredInterface::class);
+        $required
+            ->expects($this->once())
+            ->method('getRequiredVersion')
+            ->willReturn('2023.1.0');
+
         $comparator = $this->createMock(ComparatorInterface::class);
         $comparator
             ->expects($this->once())
             ->method('greaterThan')
-            ->with('2023.1.0.0-dev', '2023.1.0')
+            ->with('2023.1.0', '2023.1.0')
             ->willReturn(true);
 
-        $checker = new RoadRunnerVersionChecker(checker: new VersionChecker(comparator: $comparator));
+        $checker = new RoadRunnerVersionChecker(checker: new VersionChecker(
+            installedVersion: $installed,
+            requiredVersion: $required,
+            comparator: $comparator
+        ));
 
         RoadRunner::create(versionChecker: $checker);
 
