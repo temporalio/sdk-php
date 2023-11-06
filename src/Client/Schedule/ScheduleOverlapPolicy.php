6ea0feac411404a9461472b70e83b41b20d63034
roxblnfk <roxblnfk@ya.ru>
2023-11-06 18:37:16 +0400

    Add ScheduleOverlapPolicy enum

---
+++ src/Client/Schedule/ScheduleOverlapPolicy.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Temporal\Client\Schedule;
+
+/**
+ * ScheduleOverlapPolicy controls what happens when a workflow would be started
+ * by a schedule, and is already running.
+ *
+ * @see \Temporal\Api\Enums\V1\ScheduleOverlapPolicy
+ */
+enum ScheduleOverlapPolicy: int
+{
+    case Unspecified = 0;
+
+    /**
+     * (default)
+     * Means don't start anything. When the workflow completes, the next scheduled event after that
+     * time will be considered.
+     */
+    case Skip = 1;
+
+    /**
+     * Means start the workflow again soon as the current one completes, but only buffer one start in this way.
+     * If another start is supposed to happen when the workflow is running, and one is already buffered,
+     * then only the first one will be started after the running workflow finishes.
+     */
+    case BufferOne = 2;
+
+    /**
+     * Means buffer up any number of starts to all happen sequentially, immediately after the running
+     * workflow completes.
+     */
+    case BufferAll = 3;
+
+    /**
+     * Means that if there is another workflow running, cancel it, and start the new one after the old one
+     * completes cancellation.
+     */
+    case CancelOther = 4;
+
+    /**
+     * Means that if there is another workflow running, terminate it and start the new one immediately.
+     */
+    case TerminateOther = 5;
+
+    /**
+     * Means start any number of concurrent workflows. Note that with this policy, last completion result
+     * and last failure will not be available since workflows are not sequential.
+     */
+    case AllowAll = 6;
+}
