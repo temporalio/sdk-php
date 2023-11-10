22f1c0cae7658fcc38fd95566f9f3b6f9b625d27
roxblnfk <roxblnfk@ya.ru>
2023-11-11 00:14:37 +0400

    Add ScheduleList* DTOs

---
+++ src/Client/Schedule/ScheduleListEntry.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Temporal\Client\Schedule;
+
+use Temporal\DataConverter\EncodedCollection;
+use Temporal\Internal\Marshaller\Meta\Marshal;
+
+/**
+ * @see \Temporal\Api\Schedule\V1\ScheduleListEntry
+ */
+final class ScheduleListEntry
+{
+    #[Marshal(name: 'info')]
+    public readonly ScheduleListInfo $info;
+
+    /**
+     * The business identifier of the schedule.
+     */
+    #[Marshal(name: 'schedule_id')]
+    public readonly string $scheduleId;
+
+    /**
+     * Non-indexed user supplied information.
+     */
+    #[Marshal(name: 'memo')]
+    public readonly EncodedCollection $memo;
+
+    /**
+     * Indexed info that can be used in query of List schedules APIs. The key and value type must be registered on
+     * Temporal server side. Use GetSearchAttributes API to get valid key and corresponding value type.
+     * For supported operations on different server versions see {@link https://docs.temporal.io/visibility}.
+     */
+    #[Marshal(name: 'search_attributes')]
+    public readonly EncodedCollection $searchAttributes;
+}
---
+++ src/Client/Schedule/ScheduleListInfo.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Temporal\Client\Schedule;
+
+use Temporal\Client\Schedule\Spec\ScheduleSpec;
+use Temporal\Internal\Marshaller\Meta\Marshal;
+use Temporal\Internal\Marshaller\Meta\MarshalArray;
+use Temporal\Workflow\WorkflowType;
+
+/**
+ * @see \Temporal\Api\Schedule\V1\ScheduleListInfo
+ */
+final class ScheduleListInfo
+{
+    #[Marshal(name: 'spec')]
+    public readonly ScheduleSpec $spec;
+
+    #[Marshal(name: 'workflow_type')]
+    public readonly WorkflowType $workflowType;
+
+    #[Marshal(name: 'notes')]
+    public readonly string $notes;
+
+    #[Marshal(name: 'paused')]
+    public readonly bool $paused;
+
+    /**
+     * @var list<ScheduleActionResult>
+     */
+    #[MarshalArray(name: 'recent_actions', of: ScheduleActionResult::class)]
+    public readonly array $recentActions;
+
+    /**
+     * Future action times
+     *
+     * @var list<\DateTimeImmutable>
+     */
+    #[MarshalArray(name: 'future_action_times', of: \DateTimeImmutable::class)]
+    public readonly array $futureActionTimes;
+}
