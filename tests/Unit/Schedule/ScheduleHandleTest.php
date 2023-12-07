a26e76072e8ae7c813527b6a465dfef704e6f831
roxblnfk <roxblnfk@ya.ru>
2023-12-07 18:56:28 +0400

    Cover ScheduleHandle::trigger(), ScheduleHandle::listScheduleMatchingTimes(), ScheduleHandle::describe(), ScheduleHandle::update()

--- tests/Unit/Schedule/ScheduleHandleTest.php
+++ tests/Unit/Schedule/ScheduleHandleTest.php
@@ -8,13 +8,20 @@ use Spiral\Attributes\AttributeReader;
 use Temporal\Api\Schedule\V1\BackfillRequest;
 use Temporal\Api\Workflowservice\V1\DeleteScheduleRequest;
 use Temporal\Api\Workflowservice\V1\DeleteScheduleResponse;
+use Temporal\Api\Workflowservice\V1\DescribeScheduleRequest;
+use Temporal\Api\Workflowservice\V1\DescribeScheduleResponse;
+use Temporal\Api\Workflowservice\V1\ListScheduleMatchingTimesRequest as ListSchedulesRequest;
+use Temporal\Api\Workflowservice\V1\ListScheduleMatchingTimesResponse;
 use Temporal\Api\Workflowservice\V1\PatchScheduleRequest;
 use Temporal\Api\Workflowservice\V1\PatchScheduleResponse;
+use Temporal\Api\Workflowservice\V1\UpdateScheduleRequest;
+use Temporal\Api\Workflowservice\V1\UpdateScheduleResponse;
 use Temporal\Client\ClientOptions;
 use Temporal\Client\GRPC\ServiceClientInterface;
 use Temporal\Client\Schedule\BackfillPeriod;
 use PHPUnit\Framework\TestCase;
 use Temporal\Client\Schedule\Policy\ScheduleOverlapPolicy;
+use Temporal\Client\Schedule\Schedule;
 use Temporal\Client\Schedule\ScheduleHandle;
 use Temporal\DataConverter\DataConverter;
 use Temporal\DataConverter\DataConverterInterface;
@@ -29,7 +36,7 @@ use Temporal\Internal\Marshaller\ProtoToArrayConverter;
  */
 class ScheduleHandleTest extends TestCase
 {
-    public function getId(): void
+    public function testGetId(): void
     {
         $scheduleHandle = $this->createScheduleHandle(id: 'test-id-test');
 
@@ -61,6 +68,32 @@ class ScheduleHandleTest extends TestCase
         $this->assertSame('default', $testContext->request->getNamespace());
         $this->assertSame('test-id', $testContext->request->getScheduleId());
         $this->assertSame('test-identity', $testContext->request->getIdentity());
+    }
+
+    public function testTrigger(): void
+    {
+        $testContext = new class {
+            public PatchScheduleRequest $request;
+        };
+        // Prepare mocks
+        $clientMock = $this->createMock(ServiceClientInterface::class);
+        $clientMock->expects($this->once())
+            ->method('PatchSchedule')
+            ->with($this->callback(fn (PatchScheduleRequest $request) => $testContext->request = $request or true))
+            ->willReturn(new PatchScheduleResponse());
+
+        $scheduleHandle = $this->createScheduleHandle(
+            client: $clientMock,
+        );
+
+        $scheduleHandle->trigger(ScheduleOverlapPolicy::AllowAll);
+
+        $this->assertTrue(isset($testContext->request));
+        $this->assertSame('default', $testContext->request->getNamespace());
+        $this->assertSame('test-id', $testContext->request->getScheduleId());
+        $triggerImmediately = $testContext->request->getPatch()->getTriggerImmediately();
+        $this->assertNotNull($triggerImmediately);
+        $this->assertSame(ScheduleOverlapPolicy::AllowAll->value, $triggerImmediately->getOverlapPolicy());
     }
 
     public function testPause(): void
@@ -170,6 +203,95 @@ class ScheduleHandleTest extends TestCase
         $this->assertSame(ScheduleOverlapPolicy::BufferAll->value, $backfill1->getOverlapPolicy());
     }
 
+    public function testUpdate(): void
+    {
+        $testContext = new class {
+            public UpdateScheduleRequest $request;
+        };
+        // Prepare mocks
+        $clientMock = $this->createMock(ServiceClientInterface::class);
+        $clientMock->expects($this->once())
+            ->method('UpdateSchedule')
+            ->with($this->callback(fn (UpdateScheduleRequest $request) => $testContext->request = $request or true))
+            ->willReturn(new UpdateScheduleResponse());
+        $scheduleHandle = $this->createScheduleHandle(client: $clientMock);
+
+        $scheduleHandle->update(Schedule::new(), 'test-conflict-token');
+
+        $this->assertTrue(isset($testContext->request));
+        $this->assertSame('default', $testContext->request->getNamespace());
+        $this->assertSame('test-id', $testContext->request->getScheduleId());
+        $this->assertSame('test-identity', $testContext->request->getIdentity());
+        $this->assertSame('test-conflict-token', $testContext->request->getConflictToken());
+        $this->assertNotNull($testContext->request->getSchedule());
+    }
+
+    public function testDescribe(): void
+    {
+        $testContext = new class {
+            public DescribeScheduleRequest $request;
+        };
+        // Prepare mocks
+        $clientMock = $this->createMock(ServiceClientInterface::class);
+        $clientMock->expects($this->once())
+            ->method('DescribeSchedule')
+            ->with($this->callback(fn (DescribeScheduleRequest $request) => $testContext->request = $request or true))
+            ->willReturn((new DescribeScheduleResponse())->setConflictToken('test-conflict-token'));
+        $scheduleHandle = $this->createScheduleHandle(
+            client: $clientMock,
+        );
+
+        $result = $scheduleHandle->describe();
+
+        $this->assertTrue(isset($testContext->request));
+        $this->assertSame('default', $testContext->request->getNamespace());
+        $this->assertSame('test-id', $testContext->request->getScheduleId());
+        // Test result
+        $this->assertSame('test-conflict-token', $result->conflictToken);
+    }
+
+    public function testListScheduleMatchingTimes(): void
+    {
+        $testContext = new class {
+            public ListSchedulesRequest $request;
+        };
+        $resultList = [
+            new \DateTimeImmutable('2021-01-01'),
+            new \DateTimeImmutable('2021-01-02'),
+            new \DateTimeImmutable('2021-01-03'),
+            new \DateTimeImmutable('2021-01-04'),
+        ];
+        // Prepare mocks
+        $clientMock = $this->createMock(ServiceClientInterface::class);
+        $clientMock->expects($this->once())
+            ->method('ListScheduleMatchingTimes')
+            ->with($this->callback(fn (ListSchedulesRequest $request) => $testContext->request = $request or true))
+            ->willReturn((new ListScheduleMatchingTimesResponse())
+                ->setStartTime(array_map(static fn(\DateTimeInterface $dateTime) => (new \Google\Protobuf\Timestamp())
+                    ->setSeconds($dateTime->getTimestamp()), $resultList))
+            );
+        $scheduleHandle = $this->createScheduleHandle(
+            client: $clientMock,
+        );
+
+        $result = $scheduleHandle->listScheduleMatchingTimes(
+            new \DateTimeImmutable('2021-01-01'),
+            new \DateTimeImmutable('2021-01-04'),
+        );
+
+        $this->assertTrue(isset($testContext->request));
+        $this->assertSame('default', $testContext->request->getNamespace());
+        $this->assertSame('test-id', $testContext->request->getScheduleId());
+        // Test ListScheduleMatchingTimesRequest
+        $this->assertSame('2021-01-01', $testContext->request->getStartTime()->toDateTime()->format('Y-m-d'));
+        $this->assertSame('2021-01-04', $testContext->request->getEndTime()->toDateTime()->format('Y-m-d'));
+        // Test result
+        $this->assertInstanceOf(\Traversable::class, $result);
+        $this->assertInstanceOf(\Countable::class, $result);
+        $this->assertCount(4, $result);
+        $this->assertEquals($resultList, \iterator_to_array($result));
+    }
+
     public function testBackfillWithWrongArguments(): void
     {
         $scheduleHandle = $this->createScheduleHandle();
@@ -187,6 +309,7 @@ class ScheduleHandleTest extends TestCase
         ?ClientOptions $clientOptions = null,
         ?DataConverterInterface $converter = null,
         ?MarshallerInterface $marshaller = null,
+        ?ProtoToArrayConverter $protoConverter = null,
         string $namespace = 'default',
         string $id = 'test-id',
     ): ScheduleHandle
@@ -200,7 +323,7 @@ class ScheduleHandleTest extends TestCase
             $clientOptions,
             $converter = ($converter ?? DataConverter::createDefault()),
             $marshaller ?? new Marshaller(new AttributeMapperFactory(new AttributeReader())),
-            new ProtoToArrayConverter($converter),
+            $protoConverter ?? new ProtoToArrayConverter($converter),
             $namespace,
             $id,
         );
