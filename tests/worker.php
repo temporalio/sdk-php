f35f0c4eaa829cfc6d717b75fba121972f00a16f
roxblnfk <roxblnfk@ya.ru>
2024-06-06 18:40:52 +0400

    Inherit TaskQueue from the parent Workflow when a Child Workflow options are created

--- tests/worker.php
+++ tests/worker.php
@@ -13,12 +13,13 @@ use Temporal\Testing\WorkerFactory;
 use Temporal\Tests\Fixtures\PipelineProvider;
 use Temporal\Tests\Interceptor\HeaderChanger;
 use Temporal\Tests\Interceptor\InterceptorCallsCounter;
+use Temporal\Worker\WorkerInterface;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 /**
  * @param string $dir
- * @return array<string>
+ * @return array<class-string>
  */
 $getClasses = static function (string $dir, string $namespace): iterable {
     $dir = realpath($dir);
@@ -41,24 +42,37 @@ $interceptors = [
     HeaderChanger::class,
 ];
 
-$worker = $factory->newWorker(
-    'default',
-    \Temporal\Worker\WorkerOptions::new()
-        ->withMaxConcurrentWorkflowTaskPollers(5),
-    interceptorProvider: new PipelineProvider($interceptors),
-);
+$workers = [
+    'default' => $factory->newWorker(
+        'default',
+        \Temporal\Worker\WorkerOptions::new()
+            ->withMaxConcurrentWorkflowTaskPollers(5),
+        interceptorProvider: new PipelineProvider($interceptors),
+    ),
+    'FooBar' => $factory->newWorker(
+        'FooBar',
+        \Temporal\Worker\WorkerOptions::new()
+            ->withMaxConcurrentWorkflowTaskPollers(5),
+    ),
+];
 
 // register all workflows
 foreach ($getClasses(__DIR__ . '/Fixtures/src/Workflow', 'Temporal\\Tests\\Workflow\\') as $class) {
-    if (class_exists($class) && !interface_exists($class)) {
-        $worker->registerWorkflowTypes($class);
+    if (class_exists($class) && !\interface_exists($class)) {
+        \array_walk(
+            $workers,
+            static fn (WorkerInterface $worker) => $worker->registerWorkflowTypes($class),
+        );
     }
 }
 
 // register all activity
 foreach ($getClasses(__DIR__ . '/Fixtures/src/Activity', 'Temporal\\Tests\\Activity\\') as $class) {
-    if (class_exists($class) && !interface_exists($class)) {
-        $worker->registerActivityImplementations(new $class());
+    if (class_exists($class) && !\interface_exists($class)) {
+        \array_walk(
+            $workers,
+            static fn (WorkerInterface $worker) => $worker->registerActivityImplementations(new $class()),
+        );
     }
 }
 
