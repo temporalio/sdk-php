b60439c292824ba29cb87d51655008ee65e100fc
roxblnfk <roxblnfk@ya.ru>
2024-07-22 22:08:44 +0400

    Move bootstrap for Functional tests file in the related dir

--- tests/Functional/bootstrap.php
+++ tests/Functional/bootstrap.php
@@ -5,11 +5,11 @@ declare(strict_types=1);
 use Temporal\Testing\Environment;
 use Temporal\Tests\SearchAttributeTestInvoker;
 
-chdir(__DIR__ . '/..');
-require __DIR__ . '/../vendor/autoload.php';
+chdir(__DIR__ . '/../..');
+require __DIR__ . '/../../vendor/autoload.php';
 
 $environment = Environment::create();
-$environment->startTemporalTestServer();
+// $environment->startTemporalTestServer();
 (new SearchAttributeTestInvoker)();
 $environment->startRoadRunner('./rr serve -c .rr.silent.yaml -w tests');
 register_shutdown_function(fn() => $environment->stop());
