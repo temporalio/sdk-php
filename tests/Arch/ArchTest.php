ed75242bb41f3b72ebedef72e3e50675432490e9
roxblnfk <roxblnfk@ya.ru>
2024-10-29 02:05:52 +0400

    Add architecture tests

---
+++ tests/Arch/ArchTest.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Temporal\Tests\Arch;
+
+use PHPUnit\Architecture\ArchitectureAsserts;
+use PHPUnit\Framework\TestCase;
+
+final class ArchTest extends TestCase
+{
+    protected array $excludedPaths = [
+        'vendor',
+        'tests',
+    ];
+
+    use ArchitectureAsserts;
+
+    public function testDontForgetDebugFunctions(): void
+    {
+        $functions = ['dump', 'trap', 'tr', 'td', 'var_dump'];
+        $layer = $this->layer();
+
+        foreach ($layer as $object) {
+            foreach ($object->uses as $use) {
+                foreach ($functions as $function) {
+                    $function === $use and throw new \Exception(
+                        \sprintf(
+                            'Function `%s()` is used in %s.',
+                            $function,
+                            $object->name,
+                        ),
+                    );
+                }
+            }
+        }
+
+        $this->assertTrue(true);
+    }
+}
