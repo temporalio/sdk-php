609fad4eae57389c8cba3fff636f97be2ccfb047
roxblnfk <roxblnfk@ya.ru>
2025-01-14 03:17:28 +0400

    Sync tests behavior with Temporal CLI 1.2.0

--- tests/Acceptance/Extra/Update/UntypedStubTest.php
+++ tests/Acceptance/Extra/Update/UntypedStubTest.php
@@ -37,7 +37,11 @@ class UntypedStubTest extends TestCase
 
         $this->assertSame(['key' => 'resolved'], (array)$result, 'Workflow result contains resolved value');
         $this->assertFalse($handle->hasResult());
-        $this->assertFalse($resolver->hasResult(), 'Resolver should not have result because of wait policy');
+
+        // Since Temporal CLI 1.2.0, the result is available immediately after the operation
+        $this->assertTrue($resolver->hasResult());
+        $this->assertSame('resolved', $resolver->getResult());
+
         // Fetch result
         $this->assertSame('resolved', $handle->getResult());
         $this->assertTrue($handle->hasResult());
