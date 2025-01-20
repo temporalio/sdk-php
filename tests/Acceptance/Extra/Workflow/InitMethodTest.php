26e308cfd7df1ef53c11a7f1851bf878724191ca
roxblnfk <roxblnfk@ya.ru>
2025-01-20 20:36:27 +0400

    Add more `initMethod` tests

--- tests/Acceptance/Extra/Workflow/InitMethodTest.php
+++ tests/Acceptance/Extra/Workflow/InitMethodTest.php
@@ -7,9 +7,11 @@ namespace Temporal\Tests\Acceptance\Extra\Workflow\InitMethod;
 use PHPUnit\Framework\Attributes\CoversFunction;
 use PHPUnit\Framework\Attributes\Test;
 use Temporal\Client\WorkflowStubInterface;
+use Temporal\Exception\Client\WorkflowFailedException;
+use Temporal\Exception\Failure\TimeoutFailure;
+use Temporal\Tests\Acceptance\App\Attribute\RetryOptions;
 use Temporal\Tests\Acceptance\App\Attribute\Stub;
 use Temporal\Tests\Acceptance\App\TestCase;
-use Temporal\Workflow;
 use Temporal\Workflow\InitMethod;
 use Temporal\Workflow\WorkflowInterface;
 use Temporal\Workflow\WorkflowMethod;
@@ -18,13 +20,41 @@ use Temporal\Workflow\WorkflowMethod;
 class InitMethodTest extends TestCase
 {
     #[Test]
-    public function updateHandlersWithOneCall(
+    public function simpleCase(
         #[Stub(
             type: 'Extra_Workflow_InitMethod',
             args: [new Input('John Doe', 30)],
         )] WorkflowStubInterface $stub,
     ): void {
-        $this->assertTrue($stub->getResult(timeout: 5));
+        $this->assertTrue($stub->getResult());
+    }
+
+    #[Test]
+    public function emptyConstructor(
+        #[Stub(
+            type: 'Extra_Workflow_InitMethod__empty_constructor',
+            args: [new Input('John Doe', 30)],
+        )] WorkflowStubInterface $stub,
+    ): void {
+        $this->assertTrue($stub->getResult());
+    }
+
+    #[Test]
+    public function differentConstructorParams(
+        #[Stub(
+            type: 'Extra_Workflow_InitMethod__different_constructor_params',
+            executionTimeout: '2 seconds',
+            args: [new Input('John Doe', 30)],
+            retryOptions: new RetryOptions(
+                maximumAttempts: 1,
+            ),
+        )] WorkflowStubInterface $stub,
+    ): void {
+        try {
+            $stub->getResult();
+        } catch (WorkflowFailedException $failure) {
+            self:self::assertInstanceOf(TimeoutFailure::class, $failure->getPrevious());
+        }
     }
 }
 
@@ -34,11 +64,48 @@ class TestWorkflow
     private array $initInput;
 
     #[InitMethod]
-    public function __construct(Input $input) {
+    public function __construct(Input $input)
+    {
         $this->initInput = \func_get_args();
     }
 
     #[WorkflowMethod(name: "Extra_Workflow_InitMethod")]
+    public function handle(Input $input)
+    {
+        return $this->initInput === \func_get_args();
+    }
+}
+
+#[WorkflowInterface]
+class TestWorkflowEmptyConstructor
+{
+    private array $initInput;
+
+    #[InitMethod]
+    public function __construct()
+    {
+        $this->initInput = \func_get_args();
+    }
+
+    #[WorkflowMethod(name: "Extra_Workflow_InitMethod__empty_constructor")]
+    public function handle(Input $input)
+    {
+        return $this->initInput === \func_get_args();
+    }
+}
+
+#[WorkflowInterface]
+class TestWorkflowDifferentConstructorParams
+{
+    private array $initInput;
+
+    #[InitMethod]
+    public function __construct(\stdClass $input)
+    {
+        $this->initInput = \func_get_args();
+    }
+
+    #[WorkflowMethod(name: "Extra_Workflow_InitMethod__different_constructor_params")]
     public function handle(Input $input)
     {
         return $this->initInput === \func_get_args();
