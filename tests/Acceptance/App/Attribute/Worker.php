4c34f742d2f89505d0563055f1d4d28ffea33fa0
roxblnfk <roxblnfk@ya.ru>
2025-04-10 16:18:23 +0400

    Tests: add #[Worker] attribute to customize worker settings

---
+++ tests/Acceptance/App/Attribute/Worker.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Temporal\Tests\Acceptance\App\Attribute;
+
+use Psr\Log\LoggerInterface;
+use Temporal\Interceptor\PipelineProvider;
+use Temporal\Worker\WorkerOptions;
+
+/**
+ * Customize worker options.
+ *
+ * The attribute can be used once per TaskQueue.
+ *
+ * @see \Temporal\Tests\Acceptance\App\Feature\WorkerFactory
+ */
+#[\Attribute(\Attribute::TARGET_CLASS)]
+final class Worker
+{
+    /**
+     * @param array|null $pipelineProvider Callable that returns {@see PipelineProvider}
+     * @param array|null $logger Callable that returns {@see LoggerInterface}
+     */
+    public function __construct(
+        public readonly ?WorkerOptions $options = null,
+        public readonly ?array $pipelineProvider = null,
+        public readonly ?array $logger = null,
+    ) {}
+}
