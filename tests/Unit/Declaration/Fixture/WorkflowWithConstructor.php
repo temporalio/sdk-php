3a31075da17b55dca5c069a72bfdcc0f670d628f
roxblnfk <roxblnfk@ya.ru>
2025-05-07 15:22:44 +0400

    Add tests

---
+++ tests/Unit/Declaration/Fixture/WorkflowWithConstructor.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * This file is part of Temporal package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Temporal\Tests\Unit\Declaration\Fixture;
+
+use Temporal\Workflow\WorkflowInterface;
+use Temporal\Workflow\WorkflowMethod;
+
+/** @WorkflowInterface */
+#[WorkflowInterface]
+class WorkflowWithConstructor
+{
+    public function __construct() {}
+
+    /**
+     * @WorkflowMethod
+     */
+    #[WorkflowMethod]
+    public function handler(): void {}
+}
