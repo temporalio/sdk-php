c00f4317b2a96fc5fb65f84e541f441d867f15be
roxblnfk <roxblnfk@ya.ru>
2025-07-21 17:04:05 +0400

    test: add Reset Worker With Signal test

--- tests/Acceptance/Extra/Stability/ResetWorkerTest.php
+++ tests/Acceptance/Extra/Stability/ResetWorkerTest.php
@@ -21,7 +21,7 @@ use Temporal\Workflow\WorkflowMethod;
 class ResetWorkerTest extends TestCase
 {
     #[Test]
-    public function resetWorker(
+    public function resetWithCancel(
         WorkflowClientInterface $client,
         Feature $feature,
     ): void {
@@ -59,18 +59,70 @@ class ResetWorkerTest extends TestCase
 
         self::fail('Workflow must fail with a canceled failure');
     }
+
+    #[Test]
+    public function resetWithSignal(
+        WorkflowClientInterface $client,
+        Feature $feature,
+    ): void {
+        # Create a Workflow stub with an execution timeout 12 seconds
+        $stub = $client->withTimeout(1)
+            ->newUntypedWorkflowStub(
+                'Extra_Stability_ResetWorker',
+                WorkflowOptions::new()
+                    ->withTaskQueue($feature->taskQueue)
+                    ->withWorkflowExecutionTimeout(12),
+            );
+
+        # Start the Workflow with a 10-second timer
+        $client->start($stub, 10);
+
+        # Query the Workflow to kill the Worker
+        try {
+            $stub->query('die');
+            self::fail('Query must fail with a timeout');
+        } catch (WorkflowServiceException $e) {
+            # Should fail with a timeout
+            self::assertInstanceOf(TimeoutException::class, $e->getPrevious());
+        }
+
+        $stub->signal('exit');
+
+        try {
+            # Workflow must be canceled
+            $result = $stub->getResult(timeout: 8);
+            self::assertSame('Signal', $result);
+        } catch (\Throwable) {
+            $this->fail('Workflow must finish successfully and no timeout must be thrown');
+        }
+
+        # Check that Side Effect was not lost
+        $found = false;
+        foreach ($client->getWorkflowHistory($stub->getExecution()) as $event) {
+            if ($event->hasMarkerRecordedEventAttributes()) {
+                $record = $event->getMarkerRecordedEventAttributes();
+                self::assertSame('SideEffect', $record->getMarkerName());
+                $found = true;
+                break;
+            }
+        }
+
+        self::assertTrue($found, 'Side Effect must be found in the Workflow history');
+    }
 }
 
 #[Workflow\WorkflowInterface]
 class TestWorkflow
 {
+    private bool $exit = false;
+
     #[WorkflowMethod('Extra_Stability_ResetWorker')]
     #[ReturnType(Type::TYPE_STRING)]
     public function expire(int $seconds = 10): \Generator
     {
-        yield Workflow::timer($seconds);
+        $isTimer = ! yield Workflow::awaitWithTimeout($seconds, fn(): bool => $this->exit);
 
-        return yield 'Timer';
+        return yield $isTimer ? 'Timer' : 'Signal';
     }
 
     #[Workflow\QueryMethod('die')]
@@ -78,5 +130,12 @@ class TestWorkflow
     {
         \sleep($sleep);
         exit(1);
+    }
+
+    #[Workflow\SignalMethod('exit')]
+    public function signal()
+    {
+        yield Workflow::uuid7();
+        $this->exit = true;
     }
 }
