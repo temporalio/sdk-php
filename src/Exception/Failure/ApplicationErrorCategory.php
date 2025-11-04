652449641260bda5fd408e4c71cc4376ddd90fb3
roxblnfk <roxblnfk@ya.ru>
2025-11-04 17:14:36 +0400

    feat: Introduce `ApplicationErrorCategory` enum

---
+++ src/Exception/Failure/ApplicationErrorCategory.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * This file is part of Temporal package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Temporal\Exception\Failure;
+
+/**
+ * Used to categorize application failures, for example, to distinguish benign errors from others.
+ *
+ * @see \Temporal\Api\Enums\V1\ApplicationErrorCategory
+ */
+enum ApplicationErrorCategory
+{
+    case Unspecified;
+
+    /**
+     * Expected application error with little/no severity.
+     */
+    case Benign;
+}
