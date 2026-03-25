d36d76a407f14b71e4ece63e49ec508c8d905efb
Massimiliano Torromeo <massimiliano.torromeo@gmail.com>
2026-03-25 11:36:02 +0100

    fix: give up early starting the testing roadrunner command if the process terminates

--- testing/src/Environment.php
+++ testing/src/Environment.php
@@ -126,7 +126,7 @@ final class Environment
         $this->temporalServerProcess->start();
 
         $deadline = \microtime(true) + (float) $commandTimeout;
-        while (!$temporalStarted && \microtime(true) < $deadline) {
+        while ($this->temporalServerProcess->isRunning() && !$temporalStarted && \microtime(true) < $deadline) {
             \usleep(10_000);
             $check = new Process([
                 $this->systemInfo->temporalCliExecutable,
@@ -217,16 +217,16 @@ final class Environment
 
         // wait for roadrunner to start
         $deadline = \microtime(true) + (float) $commandTimeout;
-        while (!$roadRunnerStarted && \microtime(true) < $deadline) {
+        while ($this->roadRunnerProcess->isRunning() && !$roadRunnerStarted && \microtime(true) < $deadline) {
             \usleep(10_000);
-            $check = new Process([$this->systemInfo->rrExecutable, 'workers', '-c', $configFile]);
+            $check = new Process(array_map(static fn ($arg) => $arg === 'serve' ? 'workers' : $arg, $rrCommand));
             $check->run();
             if (\str_contains($check->getOutput(), 'Workers of')) {
                 $roadRunnerStarted = true;
             }
         }
 
-        if (!$roadRunnerStarted) {
+        if (!$roadRunnerStarted || !$this->roadRunnerProcess->isRunning()) {
             $this->io->error(\sprintf(
                 'Failed to start until RoadRunner is ready. Status: "%s". Stderr: "%s". Stdout: "%s".',
                 $this->roadRunnerProcess->getStatus(),
